2ac5aa6a02af7858635ef02ad85eba600cae0f76
sunxi <xiaolei.sun@richeninfo.com>
2024-03-21 14:59:50 +0800

    00001

---
+++ src/main/java/com/richeninfo/controller/testController.java
@@ -0,0 +1,12 @@
+package com.richeninfo.controller;
+
+/**
+ * @auth lei
+ * @date 2024/3/21 14:37
+ */
+public class testController {
+
+    public static void main(String[] args) {
+        System.out.println("Hellow World");
+    }
+}
