9bc390f1fbab7fb1982c0fa59d40a2096d4eb704
Z1160866372 <1160866372@qq.com>
2024-05-22 15:28:14 +0800

    触点参数优化ditch

--- src/main/java/com/richeninfo/activeListener/ActivityMqListener.java
+++ src/main/java/com/richeninfo/activeListener/ActivityMqListener.java
@@ -11,6 +11,7 @@ import com.richeninfo.entity.mapper.entity.ActivityUserHistory;
 import com.richeninfo.entity.mapper.mapper.master.CommonMapper;
 import com.richeninfo.pojo.Constant;
 import com.richeninfo.pojo.PacketMq;
+import com.richeninfo.pojo.Result;
 import com.richeninfo.util.RopServiceManager;
 import lombok.extern.java.Log;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
@@ -47,8 +48,8 @@ public class ActivityMqListener {
             if (history == null || history.getStatus() == Constant.STATUS_RECEIVED || mq.getPacket()== null) {
                 return;
             }
-               /* String response_message = ropServiceManager.execute(mq.getPacket(), history.getUserId(),history.getActId());
-             Result request = JSON.parseObject(response_message, Result.class);
+            String response_message = ropServiceManager.execute(mq.getPacket(), history.getUserId(),history.getActId());
+            Result request = JSON.parseObject(response_message, Result.class);
             String code = request.getResponse().getErrorInfo().getCode();
             String resCode = request.getResponse().getRetInfo().getString("resultCode");
             if (Constant.SUCCESS_CODE.equals(code)) {
@@ -60,9 +61,9 @@ public class ActivityMqListener {
             } else {
                 history.setStatus(Constant.STATUS_RECEIVED_ERROR);
             }
-            history.setMessage(response_message);*/
-            history.setStatus(Constant.STATUS_RECEIVED);
-            history.setMessage("测试数据～");
+            history.setMessage(response_message);
+            /* history.setStatus(Constant.STATUS_RECEIVED);
+            history.setMessage("测试数据～");*/
             history.setCode(JSONObject.toJSONString(mq.getPacket()));
         } catch (Exception e) {
             e.printStackTrace();
